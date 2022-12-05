895b447f2a487fbe143c9b5aa5cb6988d888d4a7
igordbauer <igordbauer@gmail.com>
2022-12-05 11:29:51 -0300

    understanding-request-object

--- first classes/app.js
+++ first classes/app.js
@@ -2,6 +2,7 @@ const http = require('http')
 
 
 const server = http.createServer((request, response) => {
-console.log(request)
+    console.log(request.url, request.method, request.headers)
+    // process.exit()
 })
 server.listen(3000)
