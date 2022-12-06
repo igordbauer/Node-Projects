07b838fed3ea54112f23d60308dfcd85d8cb1d13
igordbauer <igordbauer@gmail.com>
2022-12-05 21:55:23 -0300

    weird-way-to-use-html

--- first classes/app.js
+++ first classes/app.js
@@ -1,8 +1,14 @@
 const http = require('http')
 
 
-const server = http.createServer((request, response) => {
-    console.log(request.url, request.method, request.headers)
+const server = http.createServer((req, res) => {
+    console.log(req.method)
     // process.exit()
+    res.setHeader('Content-Type', 'text/html') 
+    res.write('<html>')
+    res.write('<head><title>My first page</title></head>')
+    res.write('<body><h1>My first page</h1></body>')
+    res.write('<html/>')
+    res.end();
 })
 server.listen(3000)
