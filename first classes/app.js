383ab86a5fc6a84000b96ec84fffec27475568e7
igordbauer <igordbauer@gmail.com>
2022-12-07 22:18:01 -0300

    the-middlewares

--- first classes/app.js
+++ first classes/app.js
@@ -1,6 +1,17 @@
 const http = require('http')
-const routes = require('./routes')
-const server = http.createServer(routes)
+const express = require('express')
 
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('in the middleware')
+    next();// continue to the next middleware
+});
+app.use((req, res, next) => {
+    console.log('another middleware')
+    res.send('<h1>hello</h1>')
+});
+
+const server = http.createServer(app)
 
 server.listen(3000)
