7eaa7f4130044df76c6a314ca15a4158346b88ff
igordbauer <igordbauer@gmail.com>
2022-12-09 16:02:48 -0300

    sending-html-pages-with-path

--- first classes/app.js
+++ first classes/app.js
@@ -1,5 +1,6 @@
 
 const http = require('http')
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express();
@@ -8,13 +9,13 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(adminRoutes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 
 
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 app.listen(3000)
