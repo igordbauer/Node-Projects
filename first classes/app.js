a3746639860a222ec927f44b84afd9d005318060
igordbauer <igordbauer@gmail.com>
2022-12-09 22:29:17 -0300

    arranje-paths

--- first classes/app.js
+++ first classes/app.js
@@ -1,21 +1,14 @@
 
-const http = require('http')
-const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express();
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const notFound = require('./routes/404')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
-
-
-
-
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
-})
+app.use(notFound)
 app.listen(3000)
