76463a80a9af13191f7d87a1791fca1f4c7786ed
igordbauer <igordbauer@gmail.com>
2022-12-10 21:55:42 -0300

    adding-static-files

--- first classes/app.js
+++ first classes/app.js
@@ -1,5 +1,6 @@
 
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 const app = express();
 
@@ -8,6 +9,7 @@ const shopRoutes = require('./routes/shop')
 const notFound = require('./routes/404')
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 app.use(notFound)
