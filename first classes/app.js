0365e9887c2b075c9389c72fe745851529998689
igordbauer <igordbauer@gmail.com>
2022-12-13 23:26:49 -0300

    store-data

--- first classes/app.js
+++ first classes/app.js
@@ -4,13 +4,13 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const app = express();
 
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const notFound = require('./routes/404')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 app.use(notFound)
 app.listen(3000)
