75d6a7391b3c6de2796eb15f14fef774669b289f
igordbauer <igordbauer@gmail.com>
2022-12-15 23:02:22 -0300

    adding-some-dynamic-content

--- first classes/app.js
+++ first classes/app.js
@@ -4,6 +4,9 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const app = express();
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const notFound = require('./routes/404')
@@ -11,6 +14,7 @@ const notFound = require('./routes/404')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminData.routes)
-app.use(shopRoutes)
+app.use(shopRoutes);
+
 app.use(notFound)
 app.listen(3000)
