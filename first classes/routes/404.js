7f06e373cda7fe6abaaf697e5380d1c1c733ff02
igordbauer <igordbauer@gmail.com>
2022-12-17 11:09:03 -0300

    adding-remaining-pug-views

--- first classes/routes/404.js
+++ first classes/routes/404.js
@@ -4,7 +4,9 @@ const path = require('path')
 const rootDir = require('../util/path')
 
 router.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).render('404',{
+        pageTitle:'Page not found'
+    })
 })
 
 module.exports = router
--- first classes/routes/admin.js
+++ first classes/routes/admin.js
@@ -6,7 +6,10 @@ const router = express.Router()
 const products = []
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    res.render('add-product',{
+        pageTitle:'Add-product'
+    })
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 router.post('/add-product', (req, res, next) => {
     products.push({ title: req.body.title })
